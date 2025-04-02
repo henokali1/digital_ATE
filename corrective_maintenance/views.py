61fc5067fcbe308518a1bc8a6222c691c69e32a0
henok <henokali1@gmail.com>
2025-04-02 13:49:46 +0400

    corrective maintenance list view filters

--- corrective_maintenance/views.py
+++ corrective_maintenance/views.py
@@ -8,37 +8,76 @@ from datetime import datetime
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
+# corrective_maintenance/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import CorrectiveMaintenance
+from .forms import CorrectiveMaintenanceForm
+from job_card.models import JobCard
+from datetime import datetime
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from location.models import Location
+from django.contrib.auth.models import User
+from asset.models import Asset
+
 # List View
 @login_required
 def maintenance_list(request):
     records = CorrectiveMaintenance.objects.all().order_by('-start_date')
 
     start_date_filter = request.GET.get('start_date')
-    end_date_filter = request.GET.get('end_date')  # Get date filters from query parameters
+    end_date_filter = request.GET.get('end_date')
+    type_filter = request.GET.get('type')
+    section_filter = request.GET.get('section')
+    location_filter = request.GET.get('location')
+    completed_by_filter = request.GET.get('completed_by')
+    asset_filter = request.GET.get('asset')
 
-    #Filtering logic
+    # Date Filtering
     if start_date_filter and end_date_filter:
-         try:
-             start_date = datetime.strptime(start_date_filter, '%Y-%m-%d').date()
-             end_date = datetime.strptime(end_date_filter, '%Y-%m-%d').date()
-             records = records.filter(start_date__range=[start_date,end_date])
-         except (ValueError, TypeError):
-               pass #Ignore if a bad date value is supplied
+        try:
+            start_date = datetime.strptime(start_date_filter, '%Y-%m-%d').date()
+            end_date = datetime.strptime(end_date_filter, '%Y-%m-%d').date()
+            records = records.filter(start_date__range=[start_date, end_date])
+        except (ValueError, TypeError):
+            pass
     elif start_date_filter:
-         try:
-             start_date = datetime.strptime(start_date_filter, '%Y-%m-%d').date()
-             records = records.filter(start_date__gte=start_date)
-         except (ValueError, TypeError):
-               pass
+        try:
+            start_date = datetime.strptime(start_date_filter, '%Y-%m-%d').date()
+            records = records.filter(start_date__gte=start_date)
+        except (ValueError, TypeError):
+            pass
     elif end_date_filter:
-         try:
-             end_date = datetime.strptime(end_date_filter, '%Y-%m-%d').date()
-             records = records.filter(start_date__lte=end_date)
-         except (ValueError, TypeError):
-               pass
-    
-    per_page = int(request.GET.get('per_page', 10)) # Get the per_page value or set to default 10
-      # Pagination
+        try:
+            end_date = datetime.strptime(end_date_filter, '%Y-%m-%d').date()
+            records = records.filter(start_date__lte=end_date)
+        except (ValueError, TypeError):
+            pass
+
+    # Type Filtering
+    if type_filter:
+        records = records.filter(type=type_filter)
+
+    # Section Filtering
+    if section_filter:
+        records = records.filter(section=section_filter)
+
+    # Location Filtering
+    if location_filter:
+        records = records.filter(location_id=location_filter)  #Filter using the id
+
+    # Completed By Filtering
+    if completed_by_filter:
+        records = records.filter(completed_by__id=completed_by_filter) #Filter using the id
+
+    # Asset Filtering
+    if asset_filter:
+        records = records.filter(asset__id=asset_filter)  #Filter using the id
+
+
+    per_page = int(request.GET.get('per_page', 10))
+
+    # Pagination
     paginator = Paginator(records, per_page)
     page = request.GET.get('page', 1)
     try:
@@ -48,7 +87,17 @@ def maintenance_list(request):
     except EmptyPage:
         maintenances = paginator.page(paginator.num_pages)
 
-    return render(request, 'corrective_maintenance/list.html', {'records': maintenances, 'per_page': per_page})
+    context = {
+        'records': maintenances,
+        'per_page': per_page,
+        'type_choices': CorrectiveMaintenance.TYPE_CHOICES,
+        'section_choices': CorrectiveMaintenance.SECTION_CHOICES,
+        'locations': Location.objects.all(),
+        'completed_by_users': User.objects.filter(userprofile__ate_staff=True),
+        'assets': Asset.objects.all(),
+    }
+
+    return render(request, 'corrective_maintenance/list.html', context)
 
 # Create View
 @login_required
